e62fc3261ac84827b74c19d459d5d0d65a0fefd0
halima-bouha <81523242+halima-bouha@users.noreply.github.com>
2022-03-17 11:32:15 +0100

    halima cooment

--- src/main/java/group1/rfid/HelloApplication.java
+++ src/main/java/group1/rfid/HelloApplication.java
@@ -11,7 +11,7 @@ import java.util.TimerTask;
 import java.util.function.Consumer;
 
 public class HelloApplication extends Application  {
-
+//comment &
     @Override
     public void start(Stage stage) throws IOException {
 
